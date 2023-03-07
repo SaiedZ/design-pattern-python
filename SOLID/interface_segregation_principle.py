052e0ef9606e1b8777891eefd812cdcdd43f189b
Deias <saied_am@hotmail.com>
2023-03-07 19:05:37 +0100

    feat(SOLID): add interface segregation principle

---
+++ SOLID/interface_segregation_principle.py
@@ -0,0 +1,149 @@
+"""
+The Interface Segregation Principle (ISP) is a principle in object-oriented design that states
+that no client should be forced to depend on methods it does not use. In other words, it
+suggests that a class or interface should have only the methods that are relevant to its
+purpose, and not force its clients to implement unnecessary methods.
+"""
+
+
+from abc import abstractmethod
+
+
+# Example 1
+
+
+class Machine:
+    def print(self, document):
+        raise NotImplementedError()
+
+    def fax(self, document):
+        raise NotImplementedError()
+
+    def scan(self, document):
+        raise NotImplementedError()
+
+
+# ok if you need a multifunction device
+class MultiFunctionPrinter(Machine):
+    def print(self, document):
+        pass
+
+    def fax(self, document):
+        pass
+
+    def scan(self, document):
+        pass
+
+
+class OldFashionedPrinter(Machine):
+    def print(self, document):
+        # ok - print stuff
+        pass
+
+    def fax(self, document):
+        pass  # do-nothing
+
+    def scan(self, document):
+        """Not supported!"""
+        raise NotImplementedError('Printer cannot scan!')
+
+
+class Printer:
+    @abstractmethod
+    def print(self, document): pass
+
+
+class Scanner:
+    @abstractmethod
+    def scan(self, document): pass
+
+
+# same for Fax, etc.
+
+class MyPrinter(Printer):
+    def print(self, document):
+        print(document)
+
+
+class Photocopier(Printer, Scanner):
+    def print(self, document):
+        print(document)
+
+    def scan(self, document):
+        pass  # something meaningful
+
+
+class MultiFunctionDevice(Printer, Scanner):  # , Fax, etc
+    @abstractmethod
+    def print(self, document):
+        pass
+
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+
+class MultiFunctionMachine(MultiFunctionDevice):
+    def __init__(self, printer, scanner):
+        self.printer = printer
+        self.scanner = scanner
+
+    def print(self, document):
+        self.printer.print(document)
+
+    def scan(self, document):
+        self.scanner.scan(document)
+
+
+printer = OldFashionedPrinter()
+printer.fax(123)  # nothing happens
+printer.scan(123)  # oops!
+
+
+# Example 2
+
+"""
+In this example, the IShape interface defines two methods: area() and perimeter().
+Each concrete class (Rectangle, Circle, and Square) implements only the methods that
+are relevant to its purpose. This way, clients of these classes only need to depend
+on the methods they use, and not on any unnecessary methods. This makes the code more
+modular, easier to understand, and easier to maintain.
+"""
+
+class IShape:
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Rectangle(IShape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+class Circle(IShape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+class Square(IShape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+    def perimeter(self):
+        return 4 * self.side
